a5e33e0339ce945b46b2787ac77851b6b50320ec
Huy Pham <huyredevil@gmail.com>
2022-05-03 22:13:22 +0700

    i18n

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import { TechApp } from './src'
+import { I18nextProvider } from 'react-i18next'
+import i18n from './src/lib/lang/translations/i18n'
 
 export default function App() {
+
   return (
-    <TechApp />
+    <I18nextProvider i18n={i18n}>
+      <TechApp />
+    </I18nextProvider>
   )
 }
